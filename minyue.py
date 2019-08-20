95fed4bd77d12d47e1fad3a138906b872b095f6f
huangwenzi <35164459+huangwenzi@users.noreply.github.com>
2019-08-20 16:39:32 +0800

    qinshi界面布局

--- minyue.py
+++ minyue.py
@@ -12,33 +12,44 @@ import modules.view.viewBase as viewBaseMd
 import modules.control.hotUp as hotUpMd
 import modules.control.mouse as mouseMd
 from modules.config.enum import mouse_enum
+import qinShi.view.mainView as QSmainViewMd
 
 # 开始运行文件
 def run():
-    # 初始化一个界面类
-    mainView = mainViewMd.MainView('modules/resource/mainView.png')
-    viewVessel = viewVesselMd.viewVessel('modules/resource/viewVessel.png')
-    viewBase = viewBaseMd.viewBase('modules/resource/viewBase.png')
+    # # 初始化一个界面类
+    # mainView = mainViewMd.MainView('modules/resource/mainView.png')
+    # viewVessel = viewVesselMd.viewVessel('modules/resource/viewVessel.png')
+    # viewBase = viewBaseMd.viewBase('modules/resource/viewBase.png')
 
-    # 设置容器包含父子结构
-    mainViewMd.MainView.add_son_view(mainView, viewVessel)
-    viewVesselMd.viewVessel.add_son_view(viewVessel, viewBase)
+    # # 设置容器包含父子结构
+    # mainViewMd.MainView.add_son_view(mainView, viewVessel)
+    # viewVesselMd.viewVessel.add_son_view(viewVessel, viewBase)
 
-    # 设置偏移
-    viewVesselMd.viewVessel.setPos(viewVessel, 5,5)
-    viewBaseMd.viewBase.setPos(viewBase, 5,5)
-    
+    # # 设置偏移
+    # viewVesselMd.viewVessel.setPos(viewVessel, 5,5)
+    # viewBaseMd.viewBase.setPos(viewBase, 5,5)
+
+
+
+    # 创建基础界面
+    mainView = mainViewMd.MainView('qinShi/resource/screen/0.jpg')
+    # 初始化界面功能
+    QSmainView = QSmainViewMd.MainView(mainView)
+
+
+    # 游戏循环
     while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
         # 界面绘制
-        mainViewMd.MainView.draw(mainView)
-        
+        # mainViewMd.MainView.draw(mainView)
+        QSmainView.main_view.draw(QSmainView.main_view)
+
         # 处理鼠标事件
         ret_mouse = mouseMd.Mouse.mouse_event()
         if ret_mouse.type == mouse_enum.click_open:
-            click_ret = mainViewMd.MainView.check_click(mainView, ret_mouse, [0,0])
+            click_ret = QSmainView.main_view.check_click(QSmainView.main_view, ret_mouse, [0,0])
             if click_ret.click_func:
                 click_ret.click_func()
 
