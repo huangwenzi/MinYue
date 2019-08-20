527884b0637750cd3e75340378344b4de6ba92f0
huangwenzi <35164459+huangwenzi@users.noreply.github.com>
2019-08-20 11:28:40 +0800

    鼠标类
    静态化方法

--- modules/control/mouse.py
+++ modules/control/mouse.py
@@ -4,7 +4,7 @@
 import pygame
 # 项目模块
 from modules.config.enum import mouse_enum
-
+import modules.control.instanceMgr as instanceMgrMd
 
 # 鼠标事件的返回
 class Postion():
@@ -15,23 +15,40 @@ class Postion():
 
 # 鼠标事件处理
 class Mouse():
+    # 左键是否置位
+    left_set = False
+    # 右键是否置位
+    right_set = False
 
+    # 初始化
     def __init__(self):
         self.left_set = False   # 左键是否置位
         self.right_set = False  # 右键是否置位
+    
+    # 获取单例
+    @staticmethod
+    def getInstance():
+        name = "Mouse"
+        ins = instanceMgrMd.instanceMgr.get_ins(name)
+        if not ins:
+            ins = Mouse()
+            instanceMgrMd.instanceMgr.set_ins(name, ins)
+        return ins
 
     # 鼠标事件
     # type : 0:无操作, 1:点击左键, 2:左键弹起
-    def mouse_event(self):
+    @staticmethod
+    def mouse_event():
+        ins = Mouse.getInstance()
         pressde = pygame.mouse.get_pressed()    # 获取鼠标按下信息
         position = pygame.mouse.get_pos()       # 获取鼠标位置
         ret = Postion()
         ret.x = position[0]
         ret.y = position[1]
         if pressde[0]:
-            self.left_set = True
+            ins.left_set = True
             ret.type = mouse_enum.click_down
-        elif pressde[0] == 0 and self.left_set == True:
-            self.left_set = False
+        elif pressde[0] == 0 and ins.left_set == True:
+            ins.left_set = False
             ret.type = mouse_enum.click_open
         return ret
